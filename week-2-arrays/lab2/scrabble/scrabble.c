44f1cba10ab3bde241a4b62252fc8362c9a170d3
bunyodabdusaidov <bunyodabdusaidov0@gmail.com>
2021-10-12 16:12:57 -0500

    added comment describing the program

--- week-2-arrays/lab2/scrabble/scrabble.c
+++ week-2-arrays/lab2/scrabble/scrabble.c
@@ -3,6 +3,10 @@
 #include <stdio.h>
 #include <string.h>
 
+/* This program determines which of two Scrabble words is worth more.
+In the game of Scrabble, players create words to score points, 
+and the number of points is the sum of the point values of each letter in the word. */
+
 // Points assigned to each letter of the alphabet
 int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
 
