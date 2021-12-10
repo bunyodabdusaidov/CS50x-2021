97799bd9c5c2e19b4824b6b51169ad9aa8022419
bunyodabdusaidov <bunyodabdusaidov0@gmail.com>
2021-12-09 19:34:59 -0600

    used regex and str methods to obtain only letters and words from text. To extract sentences, used nltk.tokenize package which, using unsupervised algorithm, divides text into list of sentences

--- week-6-python/psets/readability/readability.py
+++ week-6-python/psets/readability/readability.py
@@ -1,33 +1,32 @@
 import re
-from nltk import tokenize
+import nltk
+from nltk.tokenize import sent_tokenize
+
+nltk.download('punkt', quiet=True)  # Download tokenizer for English
+
 
 def readability():
-    text = input("Text: ")
-    words = text.split(' ')
-    sentences = list(filter(None, re.split('[.!?]', text)))
-    letters = str.join("", re.findall('\w', text))
+    """Using Coleman-Liau's formula, calculate the grade level needed to understand the text"""
 
-    print(len(letters))
-    print(len(words))
-    print(len(sentences))
+    text = input("Text: ")  # Prompt the user for input
+    words = text.split(' ')  # Get the list of words by splitting them with whitespace
+    sentences = sent_tokenize(text)  # Use sent_tokenize method from nltk package to separate text into sentences
+    letters = str.join("", re.findall('\w', text))  # Use regex to get only letters
 
-    L = float(len(letters)) / float(len(words)) * 100
-    S = float(len(sentences)) / float(len(words)) * 100
-    index = round(0.0588 * L - 0.296 * S - 15.8)
+    L = float(len(letters)) / float(len(words)) * 100  # Average number of letters per 100 words in the text
+    S = float(len(sentences)) / float(len(words)) * 100  # Average number of sentences per 100 words in the text
+    grade = round(0.0588 * L - 0.296 * S - 15.8)  # Get the grade using Coleman-Liau's formula
 
-    if index < 1:
+    # If grade is less than 1
+    if grade < 1:
         print("Before Grade 1")
-    elif index > 16:
+    # If grade is greater than 16
+    elif grade > 16:
         print("Grade 16+")
+    # If grade is between 1 and 16 (inclusive)
     else:
-        print(f"Grade {index}")
-
-    #print(L)
-    #print(S)
-    #print(index)
-    print(f"letters = {letters}")
-    print(f"words = {words}")
-    print(f"sentences = {sentences}")
+        print(f"Grade {grade}")
 
 
 readability()
+
