0105c1b55cf6326e67bf3d26ff56749b85923051
bunyodabdusaidov <bunyodabdusaidov0@gmail.com>
2021-12-10 20:56:32 -0600

    Used simple loop and string concatination to get the STR occurances

--- week-6-python/psets/dna/dna.py
+++ week-6-python/psets/dna/dna.py
@@ -1,89 +1,74 @@
 from sys import argv
 from csv import DictReader, reader
 
+
 def dna():
+    # Check for correct number of command-line arguments
     if len(argv) != 3:
         print("Usage: python dna.py data.csv sequence.txt")
         return 1
         
-    data = read_file(argv[1])
-    sequence = read_file(argv[2])
-    strs_values = {}
-
-    print(data)
-    print(sequence)
-
-    strs = []
-    for key, value in data.items():
-        for s in value:
-            strs.append(s)
-        break
+    data = read_file(argv[1])  # Get data in dictionary format
+    sequence = read_file(argv[2])  # Get sequence in string format
+    dna = count_strs(data, sequence)  # Get STRs and their occurances from sequence in dictionary format
     
-    print(strs)
-    
-    n = 0
-    while n < len(strs):
-        i = 0
-        k = len(strs[n])
-        tmp = ""
-        occur = set()
-        number = 0
-        #print("initial occurance")
-        while tmp != strs[n]:
-            tmp = sequence[i:k]
-            #print(tmp)
-            i += 1
-            k += 1
-            if k >= len(sequence):
-                break
-        
-        i -= 1
-        k -= 1
-        print(f"sequence[{i}:{k}] = {tmp}")
-        
-        #print("consequtive occurances")
-        for j in range(i, len(sequence), len(strs[n])):
-            tmp = sequence[j:k]
-            #print(tmp)
-            
-            if tmp != strs[n]:
-                occur.add(number)
-                number = 0
-            
-            k += len(strs[n])
-            number += 1
-        
-        strs_values[strs[n]] = max(occur)
-        n += 1
-    
-    print(strs_values)
-
+    # Check if dna exists in data.csv
     for name, strs in data.items():
-        if strs == strs_values:
-            #print(f"strs = {strs}")
+        # If exists, print the name to whom the dna belongs
+        if strs == dna:
             print(name)
             return 0
-    
+    # If dna doesn't exist in data.csv, print `No Match``
     print("No Match")
 
 
+def count_strs(data, sequence):
+    """
+    Count STRs in sequence (str). Use data (dict) get STR name. 
+    Return dictionary containing STRs and values from the sequence
+    """
+
+    strs_values = {}  # Dict to store STRs and its occurances
+
+    strs = data.values()  # Get values from data (dict)
+    strs = list(strs)[0]  # Get first element from list of STRs
+    strs = list(strs.keys())  # Store the STR names as a list
+
+    # Compute the longest run of consecutive repeats of the STR in sequence
+    for i in range(len(strs)):
+        count = 0  # Count repeats
+        pattern = strs[i]  # Get the pattern (STR) from strs list
+        while pattern in sequence:
+            count += 1
+            pattern += strs[i]  # Concatinate current pattern and current STR to check for existance
+        strs_values[strs[i]] = count  # Store STR and its value (count) in strs_values dictionary
+    
+    return strs_values
+    
+
 def read_file(file):
+    """Read file to dict or str depending on if it's .csv or .txt file"""
+
+    # If file type is .csv
     if file.endswith('.csv'):
-        data = {}
-
+        data = {} 
+        
+        # Read .csv file as Dictionary
         with open(argv[1], 'r') as file:
-            reader = DictReader(file)
-            fieldnames = reader.fieldnames
+            reader = DictReader(file)  # Dictionary reader
+            fieldnames = reader.fieldnames[1:]  # Get fieldnames except `name` field
 
+            # Build nested dictionary ('name':{'STR':number})
             for row in reader:
-                strs = {}
-                for i in range(1, len(fieldnames)):
-                    strs[fieldnames[i]] = int(row[fieldnames[i]])
+                strs = {}  
+                for i in range(len(fieldnames)):
+                    strs[fieldnames[i]] = int(row[fieldnames[i]])  # Store STR name as key and its number as value in strs (dict)
                 
-                data[row['name']] = strs
+                data[row['name']] = strs  # Store name as key and strs (dict) as value in data (dict)
         
         return data
     
+    # If file type is .txt
     elif file.endswith('.txt'):
         sequence = ""
 
