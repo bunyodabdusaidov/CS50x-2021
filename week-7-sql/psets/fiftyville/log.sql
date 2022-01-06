7cc0f43fc393626093567dad7b45eb51dd98fc90
bunyodabdusaidov <bunyodabdusaidov0@gmail.com>
2022-01-05 22:08:16 -0600

    tried to shorten the search terms, but couldn't accomplish it.

--- week-7-sql/psets/fiftyville/log.sql
+++ week-7-sql/psets/fiftyville/log.sql
@@ -8,7 +8,7 @@ SELECT id, transcript FROM interviews WHERE day=28 AND month=7 AND year=2021 AND
 SELECT id, caller, receiver FROM phone_calls WHERE day=28 AND month=7 AND year=2021 AND duration < 60;
 
 -- Find the license plates in bakery security logs
-SELECT license_plate FROM bakery_security_logs WHERE day=28 AND month=7 AND year=2021 AND hour=10 AND 14 < minute < 17 AND activity="exit";
+SELECT license_plate FROM bakery_security_logs WHERE day=28 AND month=7 AND year=2021 AND hour=10 AND 15 < minute < 25 AND activity="exit";
 
 -- Find the origin airport id or abbreviation in airports table using to city name 
 SELECT id FROM airports WHERE city="Fiftyville";
@@ -17,14 +17,35 @@ SELECT id FROM airports WHERE city="Fiftyville";
 SELECT id FROM flights WHERE day=29 AND month=7 AND year=2021 AND origin_airport_id=(SELECT id FROM airports WHERE city="Fiftyville") ORDER BY hour LIMIT 1;
 
 -- Find passport numbers of the callers in people table using phone_number and license plate 
-/*
+
 SELECT passport_number 
 FROM people 
 WHERE phone_number IN (SELECT caller FROM phone_calls WHERE day=28 AND month=7 AND year=2021 AND duration < 60) 
-AND license_plate IN (SELECT license_plate FROM bakery_security_logs WHERE day=28 AND month=7 AND year=2021 AND hour=10 AND 14 < minute < 17 AND activity="exit");
-*/
+AND license_plate IN (SELECT license_plate FROM bakery_security_logs WHERE day=28 AND month=7 AND year=2021 AND hour=10 AND 15 < minute < 25 AND activity="exit");
+
 -- Find passengers in passengers table using flight id and passport number
 SELECT passport_number 
 FROM passengers 
 WHERE flight_id=(SELECT id FROM flights WHERE day=29 AND month=7 AND year=2021 AND origin_airport_id=(SELECT id FROM airports WHERE city="Fiftyville") ORDER BY hour LIMIT 1)
-AND passport_number IN (SELECT passport_number FROM people WHERE phone_number IN (SELECT caller FROM phone_calls WHERE day=28 AND month=7 AND year=2021 AND duration < 60) AND license_plate IN (SELECT license_plate FROM bakery_security_logs WHERE day=28 AND month=7 AND year=2021 AND hour=10 AND 14 < minute < 17 AND activity="exit"));
+AND passport_number IN (SELECT passport_number FROM people WHERE phone_number IN (SELECT caller FROM phone_calls WHERE day=28 AND month=7 AND year=2021 AND duration < 60) 
+AND license_plate IN (SELECT license_plate FROM bakery_security_logs WHERE day=28 AND month=7 AND year=2021 AND hour=10 AND 15 < minute < 25 AND activity="exit"));
+
+-- Find the name and phone_number in people table using shortened list of passport numbers
+
+SELECT name, phone_number
+FROM people 
+WHERE passport_number IN (SELECT passport_number 
+FROM passengers 
+WHERE flight_id=(SELECT id FROM flights WHERE day=29 AND month=7 AND year=2021 AND origin_airport_id=(SELECT id FROM airports WHERE city="Fiftyville") ORDER BY hour LIMIT 1)
+AND passport_number IN (SELECT passport_number FROM people WHERE phone_number IN (SELECT caller FROM phone_calls WHERE day=28 AND month=7 AND year=2021 AND duration < 60) 
+AND license_plate IN (SELECT license_plate FROM bakery_security_logs WHERE day=28 AND month=7 AND year=2021 AND hour=10 AND 15 < minute < 25 AND activity="exit")));
+
+-- Shorten the list of people using name and phone number
+
+SELECT caller, receiver 
+FROM phone_calls 
+WHERE day=28 AND month=7 AND year=2021 AND duration < 60
+AND caller IN (SELECT phone_number
+FROM people WHERE passport_number IN (SELECT passport_number FROM passengers WHERE flight_id=(SELECT id FROM flights WHERE day=29 AND month=7 AND year=2021 AND origin_airport_id=(SELECT id FROM airports WHERE city="Fiftyville") ORDER BY hour LIMIT 1)
+AND passport_number IN (SELECT passport_number FROM people WHERE phone_number IN (SELECT caller FROM phone_calls WHERE day=28 AND month=7 AND year=2021 AND duration < 60) 
+AND license_plate IN (SELECT license_plate FROM bakery_security_logs WHERE day=28 AND month=7 AND year=2021 AND hour=10 AND 15 < minute < 25 AND activity="exit"))));
