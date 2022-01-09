6e688af079b75048b4336eb1c7235eae67eb4fc6
bunyodabdusaidov <bunyodabdusaidov0@gmail.com>
2022-01-08 22:33:20 -0600

    found the destination city

--- week-7-sql/psets/fiftyville/log.sql
+++ week-7-sql/psets/fiftyville/log.sql
@@ -32,7 +32,7 @@ AND license_plate IN (SELECT license_plate FROM bakery_security_logs WHERE day=2
 
 -- Find the name and phone_number in people table using shortened list of passport numbers
 
-SELECT name, phone_number
+SELECT id, name, phone_number
 FROM people 
 WHERE passport_number IN (SELECT passport_number 
 FROM passengers 
@@ -49,3 +49,100 @@ AND caller IN (SELECT phone_number
 FROM people WHERE passport_number IN (SELECT passport_number FROM passengers WHERE flight_id=(SELECT id FROM flights WHERE day=29 AND month=7 AND year=2021 AND origin_airport_id=(SELECT id FROM airports WHERE city="Fiftyville") ORDER BY hour LIMIT 1)
 AND passport_number IN (SELECT passport_number FROM people WHERE phone_number IN (SELECT caller FROM phone_calls WHERE day=28 AND month=7 AND year=2021 AND duration < 60) 
 AND license_plate IN (SELECT license_plate FROM bakery_security_logs WHERE day=28 AND month=7 AND year=2021 AND hour=10 AND 15 < minute < 25 AND activity="exit"))));
+
+-- Shorten the list of potential criminals in atm transactions list using account numbers, location, time, and transaction type
+SELECT account_number
+FROM atm_transactions
+WHERE day=28 AND month=7 AND year=2021 AND transaction_type="withdraw" AND atm_location="Leggett Street"
+AND account_number IN (SELECT account_number 
+FROM bank_accounts 
+WHERE person_id IN (SELECT id
+FROM people 
+WHERE passport_number IN (SELECT passport_number 
+FROM passengers 
+WHERE flight_id=(SELECT id FROM flights WHERE day=29 AND month=7 AND year=2021 AND origin_airport_id=(SELECT id FROM airports WHERE city="Fiftyville") ORDER BY hour LIMIT 1)
+AND passport_number IN (SELECT passport_number FROM people WHERE phone_number IN (SELECT caller FROM phone_calls WHERE day=28 AND month=7 AND year=2021 AND duration < 60) 
+AND license_plate IN (SELECT license_plate FROM bakery_security_logs WHERE day=28 AND month=7 AND year=2021 AND hour=10 AND 15 < minute < 25 AND activity="exit")))));
+
+-- Find the person id in bank_accounts table using account_number
+SELECT person_id
+FROM bank_accounts
+WHERE account_number IN (SELECT account_number 
+FROM bank_accounts 
+WHERE person_id IN (SELECT id
+FROM people 
+WHERE passport_number IN (SELECT passport_number 
+FROM passengers 
+WHERE flight_id=(SELECT id FROM flights WHERE day=29 AND month=7 AND year=2021 AND origin_airport_id=(SELECT id FROM airports WHERE city="Fiftyville") ORDER BY hour LIMIT 1)
+AND passport_number IN (SELECT passport_number FROM people WHERE phone_number IN (SELECT caller FROM phone_calls WHERE day=28 AND month=7 AND year=2021 AND duration < 60) 
+AND license_plate IN (SELECT license_plate FROM bakery_security_logs WHERE day=28 AND month=7 AND year=2021 AND hour=10 AND 15 < minute < 25 AND activity="exit")))));
+
+-- Find the name, phone_number, passport_number, license_plate in people table using id
+SELECT name
+FROM people 
+WHERE id IN (SELECT person_id
+FROM bank_accounts
+WHERE account_number IN (SELECT account_number 
+FROM bank_accounts 
+WHERE person_id IN (SELECT id
+FROM people 
+WHERE passport_number IN (SELECT passport_number 
+FROM passengers 
+WHERE flight_id=(SELECT id FROM flights WHERE day=29 AND month=7 AND year=2021 AND origin_airport_id=(SELECT id FROM airports WHERE city="Fiftyville") ORDER BY hour LIMIT 1)
+AND passport_number IN (SELECT passport_number FROM people WHERE phone_number IN (SELECT caller FROM phone_calls WHERE day=28 AND month=7 AND year=2021 AND duration < 60) 
+AND license_plate IN (SELECT license_plate FROM bakery_security_logs WHERE day=28 AND month=7 AND year=2021 AND hour=10 AND 15 < minute < 25 AND activity="exit"))))));
+
+-- Find flight_id in passengers table using passport_number
+SELECT flight_id 
+FROM passengers
+WHERE passport_number IN (SELECT passport_number
+FROM people 
+WHERE id IN (SELECT person_id
+FROM bank_accounts
+WHERE account_number IN (SELECT account_number 
+FROM bank_accounts 
+WHERE person_id IN (SELECT id
+FROM people 
+WHERE passport_number IN (SELECT passport_number 
+FROM passengers 
+WHERE flight_id=(SELECT id FROM flights WHERE day=29 AND month=7 AND year=2021 AND origin_airport_id=(SELECT id FROM airports WHERE city="Fiftyville") ORDER BY hour LIMIT 1)
+AND passport_number IN (SELECT passport_number FROM people WHERE phone_number IN (SELECT caller FROM phone_calls WHERE day=28 AND month=7 AND year=2021 AND duration < 60) 
+AND license_plate IN (SELECT license_plate FROM bakery_security_logs WHERE day=28 AND month=7 AND year=2021 AND hour=10 AND 15 < minute < 25 AND activity="exit")))))));
+
+-- Find all the destination city in flights table using flight id
+SELECT city
+FROM airports 
+WHERE id=(SELECT destination_airport_id 
+FROM flights 
+WHERE id=(SELECT flight_id 
+FROM passengers
+WHERE passport_number IN (SELECT passport_number
+FROM people 
+WHERE id IN (SELECT person_id
+FROM bank_accounts
+WHERE account_number IN (SELECT account_number 
+FROM bank_accounts 
+WHERE person_id IN (SELECT id
+FROM people 
+WHERE passport_number IN (SELECT passport_number 
+FROM passengers 
+WHERE flight_id=(SELECT id FROM flights WHERE day=29 AND month=7 AND year=2021 AND origin_airport_id=(SELECT id FROM airports WHERE city="Fiftyville") ORDER BY hour LIMIT 1)
+AND passport_number IN (SELECT passport_number FROM people WHERE phone_number IN (SELECT caller FROM phone_calls WHERE day=28 AND month=7 AND year=2021 AND duration < 60) 
+AND license_plate IN (SELECT license_plate FROM bakery_security_logs WHERE day=28 AND month=7 AND year=2021 AND hour=10 AND 15 < minute < 25 AND activity="exit")))))))));
+
+-- Find the name of the caller in people table using id and phone number in phone_calls table
+SELECT caller 
+FROM phone_calls
+WHERE caller IN (SELECT phone_number
+FROM people 
+WHERE id IN (SELECT person_id
+FROM bank_accounts
+WHERE account_number IN (SELECT account_number 
+FROM bank_accounts 
+WHERE person_id IN (SELECT id
+FROM people 
+WHERE passport_number IN (SELECT passport_number 
+FROM passengers 
+WHERE flight_id=(SELECT id FROM flights WHERE day=29 AND month=7 AND year=2021 AND origin_airport_id=(SELECT id FROM airports WHERE city="Fiftyville") ORDER BY hour LIMIT 1)
+AND passport_number IN (SELECT passport_number FROM people WHERE phone_number IN (SELECT caller FROM phone_calls WHERE day=28 AND month=7 AND year=2021 AND duration < 60) 
+AND license_plate IN (SELECT license_plate FROM bakery_security_logs WHERE day=28 AND month=7 AND year=2021 AND hour=10 AND 15 < minute < 25 AND activity="exit")))))));
