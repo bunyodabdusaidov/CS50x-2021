3b513fb1c982f1f2bcc54c06fe10889b2488ca61
bunyodabdusaidov <bunyodabdusaidov0@gmail.com>
2022-02-09 19:03:04 -0600

    Wrote quiries to solve the mystery

--- week-7-sql/psets/fiftyville/log.sql
+++ week-7-sql/psets/fiftyville/log.sql
@@ -1,36 +1,105 @@
--- Find the crime description in crime scene reports table using the time and place 
-SELECT description FROM crime_scene_reports WHERE day=28 AND month=7 AND year=2021 AND street="Humphrey Street";
+/*
+-- Get the description of the crime
+SELECT description 
+FROM crime_scene_reports 
+WHERE year=2021 AND month=7 AND day=28 AND street="Humphrey Street";
 
--- Find the transcripts in interviews table which mention bakery 
-SELECT id, transcript FROM interviews WHERE day=28 AND month=7 AND year=2021 AND transcript LIKE "%bakery%";
+-- Get the interview descriptions that mention bakery
+SELECT id, transcript
+FROM interviews
+WHERE year=2021 AND month=7 AND day=28 AND transcript LIKE "%bakery%";
 
--- Get the names of the people having the same license plate on the bakery security cameras
+-- Get the names of the people who have the same license plate as in the security logs
 SELECT name 
 FROM people
 JOIN bakery_security_logs ON people.license_plate=bakery_security_logs.license_plate
-WHERE day=28 AND month=7 AND year=2021 AND hour=10 AND 15<=minute<25
-AND activity="exit";
+WHERE year=2021 AND month=7 AND day=28 AND activity="exit" AND hour=10 AND minute >= 15 AND minute <= 25;
 
--- Get the names of the people having the same bank account number used to withdraw money from ATM 
-
+-- Get the names of the people who withdrew money in ATM in Leggett Street
 SELECT DISTINCT name
 FROM people
 JOIN bank_accounts ON people.id=bank_accounts.person_id
 JOIN atm_transactions ON bank_accounts.account_number=atm_transactions.account_number
-WHERE day=28 AND month=7 AND year=2021
-AND transaction_type="withdraw" 
-AND atm_location="Leggett Street";
+WHERE year=2021 AND month=7 AND day=28 AND atm_location="Leggett Street" AND transaction_type="withdraw";
 
--- Get the names of the people who took the first flight on the 29th
+-- Get the names of the people who made a call on the day of theft
+SELECT name
+FROM people
+JOIN phone_calls ON people.phone_number=phone_calls.caller
+WHERE year=2021 AND month=7 AND day=28 AND duration < 60;
+
+-- Get the names of the people who took the earliest flight out of Fiftyville
 
 SELECT name
 FROM people
-JOIN passengers ON people.passport_number=passengers.passport_number
-WHERE flight_id=(
-    SELECT id
+JOIN passengers ON passengers.passport_number=people.passport_number
+JOIN flights ON passengers.flight_id=flights.id
+WHERE flights.id = (
+    SELECT id 
     FROM flights
-    WHERE day=29 AND month=7 AND year=2021
-    ORDER BY hour, minute
-    LIMIT 1);
+    WHERE year=2021 AND month=7 AND day=29 AND origin_airport_id=(SELECT id FROM airports WHERE city="Fiftyville")
+    ORDER BY hour, minute);
+    --LIMIT 1);
+*/
+-- Combine all the name quiries to find the unique person
+SELECT name 
+FROM people
+JOIN bakery_security_logs ON people.license_plate=bakery_security_logs.license_plate
+WHERE year=2021 AND month=7 AND day=28 AND activity="exit" AND hour=10 AND minute >= 15 AND minute <= 25
+INTERSECT
+SELECT DISTINCT name
+FROM people
+JOIN bank_accounts ON people.id=bank_accounts.person_id
+JOIN atm_transactions ON bank_accounts.account_number=atm_transactions.account_number
+WHERE year=2021 AND month=7 AND day=28 AND atm_location="Leggett Street" AND transaction_type="withdraw"
+INTERSECT
+SELECT name
+FROM people
+JOIN phone_calls ON people.phone_number=phone_calls.caller
+WHERE year=2021 AND month=7 AND day=28 AND duration < 60
+INTERSECT
+SELECT name
+FROM people
+JOIN passengers ON passengers.passport_number=people.passport_number
+JOIN flights ON passengers.flight_id=flights.id
+WHERE flights.id = (
+    SELECT id 
+    FROM flights
+    WHERE year=2021 AND month=7 AND day=29 AND origin_airport_id=(SELECT id FROM airports WHERE city="Fiftyville")
+    ORDER BY hour, minute);
 
--- Get the names of the people who made
+-- Get the destination where the thief escaped to
+SELECT city 
+FROM airports
+WHERE id=(
+    SELECT destination_airport_id
+    FROM flights
+    WHERE year=2021 AND month=7 AND day=29 AND origin_airport_id=(
+        SELECT id 
+        FROM airports
+        WHERE city="Fiftyville"
+    )
+    AND id=(
+        SELECT flight_id
+        FROM passengers
+        WHERE passport_number=(
+            SELECT passport_number 
+            FROM people
+            WHERE name="Bruce"
+        )
+    )
+);
+
+-- Get the thief's accomplice by thief's phone_calls
+SELECT name 
+FROM people
+WHERE phone_number=(
+    SELECT receiver
+    FROM phone_calls
+    WHERE year=2021 AND month=7 AND day=28 AND duration<60 
+    AND caller=(
+        SELECT phone_number
+        FROM people
+        WHERE name="Bruce"
+    )
+);
